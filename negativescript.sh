78b7bafcb32bebb1a4ae63df03896a9362f43f14
Stephan Zimmermann <mail@zimmermann-stephan.de>
2019-09-03 10:59:34 +0200

    slep for a second after starting a background job

--- negativescript.sh
+++ negativescript.sh
@@ -26,6 +26,7 @@ if [ ! -d "$OUTDIR" ]
     mkdir -p "$OUTDIR"
     for i in $(ls $INDIR/*.CR2); do
       runner $i &
+      sleep 1
       while [ $(jobs -r | wc -l) -ge $MAX_JOBS ]; do sleep 1; done
     done
     echo "Wating for background processes to finish ..."
